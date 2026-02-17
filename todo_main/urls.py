02c449ba919dc7481bc79d6c8d99414098c7df23
Ibrahim Ibrahim <eebrahiim04@gmail.com>
2026-02-17 17:47:34 +0100

    feat: implement task creation form with CSRF protection

--- todo_main/urls.py
+++ todo_main/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from . import views
 
-urlpatterns = [path("admin/", admin.site.urls), path("", views.home, name="home")]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    # TODO
+    path("todo/", include("todo.urls")),
+]
